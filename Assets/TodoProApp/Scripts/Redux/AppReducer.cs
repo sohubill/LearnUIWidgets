c1b09472d82229a8d3892d4adf3f7a2f9e0baf98
gongjiangang <sohubill@qq.com>
2019-11-04 08:42:26 +0800

    fix

---
+++ Assets/TodoProApp/Scripts/Redux/AppReducer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoProApp
+{
+    public class AppReducer
+    {
+        public static AppState Reduce(AppState state,object action)
+        {
+            switch (action)
+            {
+                case AddTodoAction add:
+                    state.Todos.Add(add.Todo);
+                    break;
+                case RemoveTodoAction remove:
+                    state.Todos.Remove(remove.Todo);
+                    break;
+                case CompleteTodoAction complete:
+                    complete.Todo.Status = TodoStatus.Complete;
+                    break;
+                case ApplyFilterAcion filter:
+                    state.Filter = filter.Filter;
+                    break;
+            }
+            return state;
+        }
+    }
+}
